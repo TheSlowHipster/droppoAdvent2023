d495665b97588dd597779456844da00328320fc3
JemmaMac <garnetdroppo@gmail.com>
2023-12-07 18:48:37 -0500

    Commented day 1 solution, and finished day 2

--- day1.py
+++ day1.py
@@ -1,7 +1,6 @@
 import sys
 from typing import Tuple
 
-
 DIGIT_STRINGS = [
     "zero",
     "one",
@@ -17,64 +16,102 @@ DIGIT_STRINGS = [
 
 inputs = []
 
+"""
+Create a list of inputs stored globally
+"""
 def parseInputs(fname: str):
     with open(fname,"r") as f:
         for line in f:
             inputs.append(line)
 
+"""
+Return the int value of a digit character (assumed to be first in string)
+"""
 def atoi(character: str) -> int:
-    return (ord(character) - 48)
+    return (ord(character[0]) - 48)
 
+"""
+Iterates through the first and last indices to find single digit integers to
+  generate a two-digit result
+"""
 def findValueInt(line: str) -> int:
     value = 0
     foundFirst = False
     idx = 0
+
+    # Invariant: First integer hasn't been found and more line remains
     while not foundFirst and idx < len(line) - 1:
+        # If the current character is a digit it becomes the 10s place
         if line[idx].isdigit():
             value += 10*atoi(line[idx])
             foundFirst = True
         idx += 1
+
+    # Reset the index to end of the line
     idx = len(line) - 1
     foundLast = False
+
+    # Invariant: Last integer hasn't been found and more line remains
     while not foundLast and idx >= 0:
+        # If the current character is a digit it becomes the 1s place
         if line[idx].isdigit():
             value += atoi(line[idx])
             foundLast = True
         idx -= 1
+    
+    # Return the found value
     return value
 
+"""
+Iterates through a string starting at idx appending characters until either the
+  digit was found or the substring no longer matches the digit being searched.
+"""
 def getDigitForwards(line: str, idx: int, digit: str) -> Tuple[bool, int]:
     tmpIdx = idx
     test = line[idx]
+    # Invariant: test is a substring of digit
     while tmpIdx < len(line) - 2 and test in digit:
         tmpIdx+=1
         test += line[tmpIdx]
+    # Test is either digit & an extra char or it isn't the digit
     if test[:-1] == digit:
-        #print(f"Forwards found {test}\nChecking for: {digit}")
-        #print("Passed")
         return True, DIGIT_STRINGS.index(digit)
     else:
         return False, 0
 
+"""
+Iterates through a string starting at idx prepending characters until either the
+  digit was found or the substring no longer matches the digit searched.
+"""
 def getDigitBackwards(line: str, idx: int, digit: str) -> Tuple[bool, int]:
     tmpIdx = idx
     test = line[idx]
+    # Invariant: test is a substring of digit
     while tmpIdx >= 0 and test in digit:
         tmpIdx -= 1
         test = line[tmpIdx] + test
+    # Test is either digit & an extra char or it isn't the digit
     if test[1:] == digit:
         return True, DIGIT_STRINGS.index(digit)
     else:
         return False, 0
 
+
+"""
+Iterates through the first and last indices to find single digit integers or
+  their word representations to generate a two-digit result
+"""
 def findValueStr(line: str) -> int:
     value: int = 0
     idx: int = 0
     foundFirst: bool = False
+    # Invariant: First integer hasn't been found and more line remains
     while not foundFirst and idx < len(line) - 1:
+        # If it's a digit we're done
         if line[idx].isdigit():
             value += 10 * atoi(line[idx])
             foundFirst = True
+        # Else we need to parse for a digit substring
         else:
             for digit in DIGIT_STRINGS:
                 if line[idx] in digit and not foundFirst:
@@ -85,10 +122,13 @@ def findValueStr(line: str) -> int:
 
     idx = len(line) - 1
     foundLast: bool = False
+    # Invariant: Last integer hasn't been found and more line remains
     while not foundLast and idx >= 0:
+        # If it's a digit we're done
         if line[idx].isdigit():
             value += atoi(line[idx])
             foundLast = True
+        # Else we need to parse for a digit substring
         else:
             for digit in DIGIT_STRINGS:
                 if line[idx] in digit and not foundLast:
@@ -99,25 +139,34 @@ def findValueStr(line: str) -> int:
     print(f"Value found: {value}\n")
     return value
 
+"""
+Finds the sum of all two-digit integers found by the rules of the A part of the
+  puzzle
+"""
 def sumValuesInt() -> int:
     sum = 0
     for line in inputs:
-        print(findValueInt(line))
         sum += findValueInt(line)
     return sum
 
+"""
+Finds the sum of all two-digit integers found by the rules of the B part of the
+  puzzle
+"""
 def sumValuesStr() -> int:
     sum = 0
     for line in inputs:
-        # print(findValueStr(line))
         sum += findValueStr(line)
     return sum
 
+"""
+Main functionality
+"""
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        fname = "smallTest.txt"
+        fname = "smallTest1.txt"
     else:
         fname = sys.argv[1]
     parseInputs(fname)
-    # print(sumValuesInt())
+    print(sumValuesInt())
     print(sumValuesStr())
--- day2.py
+++ day2.py
@@ -1,15 +1,90 @@
 import sys
+from typing import List, Tuple, Dict
 
-inputs = []
+class Game:
+    game_id: int
+    moves_red: List[int]
+    moves_green: List[int]
+    moves_blue: List[int]
+
+    def __init__(self, id: int, moves_red: List[int], moves_green: List[int],
+                 moves_blue: List[int]) -> None:
+        self.game_id = id
+        self.moves_red = moves_red
+        self.moves_green = moves_green
+        self.moves_blue = moves_blue
+    
+    def __str__(self) -> str:
+        return f"Red:   {*self.moves_red,}\nGreen: {*self.moves_green,}\nBlue:  {*self.moves_blue,}"
+
+    def findMaxCubes(self) -> Tuple[int, int , int]:
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+        for game in self.moves_red:
+            if game > max_red:
+                max_red = game
+        for game in self.moves_green:
+            if game > max_green:
+                max_green = game
+        for game in self.moves_blue:
+            if game > max_blue:
+                max_blue = game
+        return max_red, max_green, max_blue
+    
+    def canPlay(self, red: int, green: int, blue: int) -> bool:
+        max_red, max_green, max_blue = self.findMaxCubes()
+        return max_red <= red and max_green <= green and max_blue <= blue
+
+    def power(self):
+        red_pow, green_pow, blue_pow = self.findMaxCubes()
+        return red_pow * green_pow * blue_pow
+
+games: Dict[int, Game] = {}
 
 def parseInputs(fname: str):
     with open(fname,"r") as f:
         for line in f:
-            inputs.append(line)
+            id: int = int(line.split(" ")[1][:-1])
+            moveslist: List[str] = line.split(":")[1].split(";")
+            red_moves: List[int] = []
+            green_moves: List[int] = []
+            blue_moves: List[int] = []
+            num_moves: int = 0
+            for subgame in moveslist:
+                moves: List[str] = subgame.split(",")
+                num_moves += 1
+                for move in moves:
+                    if "red" in move:
+                        red_moves.append(int(move.split(" ")[1]))
+                    elif "green" in move:
+                        green_moves.append(int(move.split(" ")[1]))
+                    elif "blue" in move:
+                        blue_moves.append(int(move.split(" ")[1]))
+                if len(red_moves) < num_moves:
+                    red_moves.append(0)
+                if len(green_moves) < num_moves:
+                    green_moves.append(0)
+                if len(blue_moves) < num_moves:
+                    blue_moves.append(0)
+                
+            games[id] = Game(id, red_moves, green_moves, blue_moves)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        fname = "smallTest.txt"
+        fname = "smallTest2.txt"
     else:
         fname = sys.argv[1]
+    max_reds = 12
+    max_green = 13
+    max_blue = 14
+    parseInputs(fname)
+    idValue = 0
+    powValue = 0
+    for game in games.values():
+        if game.canPlay(max_reds, max_green, max_blue):
+            idValue += game.game_id
+        powValue += game.power()
+    print(idValue)
+    print(powValue)
